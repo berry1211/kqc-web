0cba3ad78b61e24bcdcfbd7dd401d26b76aeff8e
Ha10 <horiiortho5@gmail.com>
2017-06-20 16:58:20 +0900

    Add comments to describe how to use index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Hello from '@/components/common/Hello'
 import Information from '@/components/members/Information'
 import Rules from '@/components/members/Rules'
-import ClubMembers from '@/components/members/ClubMember'
 
 // 共通要素
 import Footer from '@/components/common/Footer'
@@ -11,9 +10,25 @@ import Footer from '@/components/common/Footer'
 // グローバルメニュー(常に表示されているメニュー)要素
 import Schedule from '@/components/menu/Schedule'
 import Intro from '@/components/menu/Introduction'
+import ClubMembers from '@/components/members/ClubMember'
 
 Vue.use(Router)
 
+/** ここではルーティングを行っています
+*   path: 実際にアクセスされるときの相対パス
+*   name: 名前。割りと適当に決めているが、importしたときと同じ名前のほうがわかりやすい
+*   components: その画面内にどんなコンポーネントがあるかを示している。たとえば、どの画面にもフッターは存在するので、
+*               どのcomponentsにもフッターは記述されている。
+*
+*   ========== ATTENTION ==========
+*   componentsに記述される要素は、以下のルールに従います
+*
+*       component-name: imported-name
+*
+*   component-name : App.vueなどの親要素において、router-viewタグのnameで指定された名前
+*   imported-name  : このindex.jsなどでimportされた際に決めた名前
+*
+*/
 export default new Router({
   routes: [
     {
