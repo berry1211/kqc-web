723c772ca47bf274c763c1141424724d3872b64a
Ha10 <horiiortho5@gmail.com>
2017-07-22 00:57:49 +0900

    fix the style of Github Ribbon

--- src/components/developer/ForDeveloper.vue
+++ src/components/developer/ForDeveloper.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="develper">
-  <style>#forkongithub a{background:#000;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:absolute;display:block;top:0;right:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:60px;right:-60px;transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style><span id="forkongithub"><a href="https://github.com/HALU5071/kqc-web">Fork me on GitHub</a></span>
+    <span id="forkongithub"><a href="https://github.com/HALU5071/kqc-web" target="_blank">Fork me on GitHub</a></span>
     <div class="developer-wrapper">
       <div class="cover-image-wrapper">
         <div class="cover-image-container">
@@ -99,6 +99,57 @@ export default {
 }
 
 @media screen and (min-width: 480px) {
+  #forkongithub a{
+    background:#000;
+    color:#fff;
+    text-decoration:none;font-family:arial,sans-serif;
+    text-align:center;
+    font-weight:bold;
+    padding:5px 40px;
+    font-size:1rem;
+    line-height:2rem;
+    position:relative;
+    transition:0.5s;
+  }
+  #forkongithub a:hover{
+    background:#c11;
+    color:#fff;
+  }
+  #forkongithub a::before,#forkongithub a::after{
+    content:"";
+    width:100%;
+    display:block;position:absolute;
+    top:1px;
+    left:0;
+    height:1px;
+    background:#fff;
+  }
+  #forkongithub a::after{
+    bottom:1px;top:auto;
+  }
+  @media screen and (min-width:800px){
+    #forkongithub{
+      position:absolute;display:block;
+      top:0;
+      right:0;
+      width:200px;
+      overflow:hidden;
+      height:200px;
+      z-index:9999;
+    }
+    #forkongithub a{
+      width:200px;
+      position:absolute;
+      top:60px;
+      right:-60px;
+      transform:rotate(45deg);
+      -webkit-transform:rotate(45deg);
+      -ms-transform:rotate(45deg);
+      -moz-transform:rotate(45deg);
+      -o-transform:rotate(45deg);
+      box-shadow:4px 4px 10px rgba(0,0,0,0.8);
+    }
+  }
   #developer{
     height: auto;
   }
